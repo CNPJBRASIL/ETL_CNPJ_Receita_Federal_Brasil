0937b9e7f4d978acd76747b7151abda0da552c48
GERALDOMAGELAFRAGA <geraldomagelafraga@outlook.com>
2023-04-09 15:32:33 -0300

    Create get_model() in class Model issue #6

--- src/data_process.py
+++ src/data_process.py
@@ -8,21 +8,41 @@ class Model:
     def __init__(self) -> None:
 
         self.empresas = ['cnpj_base', 'nome', 'natureza_juridica', 'qualificacao', 'capital_social', 'porte', 'ente']
-
         self.estabelecimentos = ['cnpj_base', 'cnpj_ordem', 'cnpj_dv', 'identificador', 'nome_fantasia', 'data_situacao_cadastral', 'motivo_situacao_cadastral', 'cidade_exterior', 'pais', 'data_inicio_atividade', 'cnae_fiscal_principal', 'cnae_fiscal_secundaria', 'tipo_logradouro', 'logradouro', 'numero', 'complemento', 'bairro', 'cep', 'uf', 'municipio', 'ddd1', 'telefone1', 'ddd2', 'telefone2', 'ddd_fax', 'fax', 'correio_eletronico', 'situacao_especial', 'data_situacao_especial']
-
         self.simples = ['cnpj_base', 'opcao_simples', 'data_opcao_simples', 'data_exclusao_simples', 'opcao_mei', 'data_opcao_mei', 'data_exclusao_mei']
-
         self.socios = ['cnpj_base','identificador_socio','nome_socio','cpf_cnpj','qualificao_socio','data_entrada','pais','representante_legal','nome_representante','qualificacao_representante_legal','faixa_etaria']
-
         self.paises = ['codigo','descricao']
-
         self.municipios = ['codigo','descricao']
-
         self.natureza_juridica = ['codigo','descricao']
-
         self.cnae = ['codigo','descricao']
-        
+
+    def get_model(self, file_name):
+        category = ''
+        if file_name.startswith("Empresas"):
+            category = 'empresas'
+        elif file_name.startswith("Estabelecimentos"):
+            category = 'estabelecimentos'
+        elif file_name.startswith('Simples'):
+            category = 'simples'
+        elif file_name.startswith('Socios'):
+            category = 'socios'
+        elif file_name.startswith('Paises'):
+            category = 'paises'
+        elif file_name.startswith('Municipios'):
+            category = 'municipios'
+        elif file_name.startswith('Naturezas'):
+            category = 'natureza_juridica'
+        elif file_name.startswith('Cnae'):
+            category = 'cnae'
+
+        return {'table':category, 'columns':self.__dict__[category]}
+    
+
+class Process:
+    def __init__(self, dir) -> None:
+        self.dir = dir
+
+
 
 
 class Check:
@@ -31,7 +51,7 @@ class Check:
         self.col = self.model.empresas
         self.file = r"D:\CNPJ\zip_2023-03-15\Empresas0.zip"
         self.dir = r"D:\CNPJ\zip_2023-03-15"
-        self.df = pd.read_csv(self.file, compression='zip', sep=';',low_memory=False, nrows=5,names=self.col, encoding_errors='replace',  encoding='ISO-8859-1')
+
         self.files = os.listdir(self.dir)
 
         print(self.files)
@@ -70,4 +90,7 @@ class Check:
                     
 
 if "__main__" == __name__:
-    check = Check()
+    #check = Check()
+
+    model = Model()
+    print(model.get_model('Empresas01'))
