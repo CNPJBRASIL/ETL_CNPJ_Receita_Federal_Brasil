de0b74ca463a9aad4a705e3e175dc357b18ace43
GERALDOMAGELAFRAGA <geraldomagelafraga@outlook.com>
2023-04-12 20:53:27 -0300

    Treated with multi files zip issue #5

--- src/data_process.py
+++ src/data_process.py
@@ -4,48 +4,25 @@ import os
 import sqlite3
 import time
 import re
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_BZIP2
 
 class Model:
     def __init__(self) -> None:
 
-        self.empresas = ['cnpj_base', 'nome', 'natureza_juridica', 'qualificacao', 'capital_social', 'porte', 'ente']
-        self.estabelecimentos = ['cnpj_base', 'cnpj_ordem', 'cnpj_dv', 'identificador', 'nome_fantasia', 'data_situacao_cadastral', 'motivo_situacao_cadastral', 'cidade_exterior', 'pais', 'data_inicio_atividade', 'cnae_fiscal_principal', 'cnae_fiscal_secundaria', 'tipo_logradouro', 'logradouro', 'numero', 'complemento', 'bairro', 'cep', 'uf', 'municipio', 'ddd1', 'telefone1', 'ddd2', 'telefone2', 'ddd_fax', 'fax', 'correio_eletronico', 'situacao_especial', 'data_situacao_especial']
-        self.simples = ['cnpj_base', 'opcao_simples', 'data_opcao_simples', 'data_exclusao_simples', 'opcao_mei', 'data_opcao_mei', 'data_exclusao_mei']
-        self.socios = ['cnpj_base','identificador_socio','nome_socio','cpf_cnpj','qualificao_socio','data_entrada','pais','representante_legal','nome_representante','qualificacao_representante_legal','faixa_etaria']
-        self.paises = ['codigo','descricao']
-        self.municipios = ['codigo','descricao']
-        self.natureza_juridica = ['codigo','descricao']
-        self.cnae = ['codigo','descricao']
+        self.Empresas = ['cnpj_base', 'nome', 'natureza_juridica', 'qualificacao', 'capital_social', 'porte', 'ente']
+        self.Estabelecimentos = ['cnpj_base', 'cnpj_ordem', 'cnpj_dv', 'identificador', 'nome_fantasia', 'data_situacao_cadastral', 'motivo_situacao_cadastral', 'cidade_exterior', 'pais', 'data_inicio_atividade', 'cnae_fiscal_principal', 'cnae_fiscal_secundaria', 'tipo_logradouro', 'logradouro', 'numero', 'complemento', 'bairro', 'cep', 'uf', 'municipio', 'ddd1', 'telefone1', 'ddd2', 'telefone2', 'ddd_fax', 'fax', 'correio_eletronico', 'situacao_especial', 'data_situacao_especial']
+        self.Simples = ['cnpj_base', 'opcao_simples', 'data_opcao_simples', 'data_exclusao_simples', 'opcao_mei', 'data_opcao_mei', 'data_exclusao_mei']
+        self.Socios = ['cnpj_base','identificador_socio','nome_socio','cpf_cnpj','qualificao_socio','data_entrada','pais','representante_legal','nome_representante','qualificacao_representante_legal','faixa_etaria']
+        self.Paises = ['codigo','descricao']
+        self.Municipios = ['codigo','descricao']
+        self.Naturezas = ['codigo','descricao']
+        self.Cnaes = ['codigo','descricao']
         
     def get(self, file_name):
         table  = re.sub('\d+(?=\.zip)', '', file_name).replace('%20','_').replace('.zip', '').lower()
         
         return {'table':table, 'columns':self.__dict__.get(table)}
 
-
-    def get_model(self, file_name):
-        category = ''
-        if file_name.startswith("Empresas"):
-            category = 'empresas'
-        elif file_name.startswith("Estabelecimentos"):
-            category = 'estabelecimentos'
-        elif file_name.startswith('Simples'):
-            category = 'simples'
-        elif file_name.startswith('Socios'):
-            category = 'socios'
-        elif file_name.startswith('Paises'):
-            category = 'paises'
-        elif file_name.startswith('Municipios'):
-            category = 'municipios'
-        elif file_name.startswith('Naturezas'):
-            category = 'natureza_juridica'
-        elif file_name.startswith('Cnae'):
-            category = 'cnae'
-        
-
-        return {'table':category, 'columns':self.__dict__[category]}
-    
 
 class File:
     def __init__(self, dir, file_name) -> None:
@@ -76,6 +53,21 @@ class Process:
 
         _start_time = time.time()
         print('Processo iniciado')
+
+        # verifica os arquivos zip que tenham mais de 1 arquivo compactado. Descompacta e compacta cada subarquivo
+        for file in self.files:
+            with ZipFile(f'{self.dir}\{file}', 'r') as zip:
+                sub_files = zip.namelist()
+                qt = len(sub_files)
+                if qt > 1:
+                    for sub_file in sub_files:
+                        zip.extract( sub_file, path=f'{self.dir}\\')
+                        with ZipFile(f'{self.dir}\{sub_file.replace(".csv","").replace(" ","")}.zip', 'w', compression=ZIP_BZIP2) as sub_zip:
+                            sub_zip.write(f'{self.dir}\{sub_file}')
+                        os.remove( f'{self.dir}\{sub_file}')
+
+
+
 
         for file in self.files:
             _start_time = time.time()
